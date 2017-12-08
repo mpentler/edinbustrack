d231807f10d573f342f179dad0c09f589f6d95dc
mpentler <tehhustler@hotmail.com>
2017-12-08 22:02:02 +0000

    v0.0.4 - scraping fixed (for now...)

--- edinbustrack.py
+++ edinbustrack.py
@@ -3,10 +3,6 @@
 #
 # This uses screen scraping as you appear to have to be a proper developer to
 # get access to the Edinburgh City Council's BusTracker API
-#
-# Important: This doesn't work right now and is loaded with test data. In a
-# future release I'll try and fix the scraping or I guess I'll have to find 
-# out how we get access to the API
 
 from bs4 import BeautifulSoup
 import requests
@@ -19,7 +15,6 @@ def get_bus_times(stop_id): # returns a list of expected buses at the chosen sto
 	stop_data = soup.find_all("tr", attrs={'style': None, 'class': None}) # grab every single bus entry in the table - this ID changes!
 	print stop_data
 	services = [] # service list goes in here
-#	services = [(0,"1","5"),(1,"1","14"),(2,"3","4"),(3,"3","9"),(4,"33","12"),(5,"33","24"),(6,"x","xx"),(7,"x","xx"),(8,"x","x"),(9,"xx","xx"),(10,"N25","00:24"),(11,"N25","00:54")] # some test data
 
         for row_num, row in enumerate(stop_data):
                 cols = row.find_all("td") # this will grab every column per bus
