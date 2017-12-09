d5c0369213044bb9a70261e72bdc0630f2a337b5
mpentler <tehhustler@hotmail.com>
2017-12-09 00:20:29 +0000

    v0.0.5 - example updated to use new get_stop_name function

--- shellexample.py
+++ shellexample.py
@@ -7,12 +7,13 @@ from time import sleep
 
 # setup our variables first. stop id and url are definable in case they change them
 stop_id = "36232626" # grabbed from the mybustracker website
+stop_name = get_stop_name(stop_id)
 
 while True:
 	services = get_bus_times(stop_id) # update our service list
-	os.system("clear")
+#	os.system("clear")
 
-	print "Edinburgh Bus Tracker for bus stop " + stop_id + " - CTRL-C to exit"
+	print "Next departures from " + stop_name + " - CTRL-C to exit"
         print "---------------------"
 	print "Service\t\tMins"
 	print "---------------------"
