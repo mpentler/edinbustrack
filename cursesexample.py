19fe59efc846323dbeeb16bb832f7453e6c10e9c
mpentler <tehhustler@hotmail.com>
2017-12-09 00:25:31 +0000

    v0.0.5 - example updated to use new get_stop_name function, tidied up formatting as far as curses will let me

--- cursesexample.py
+++ cursesexample.py
@@ -1,4 +1,5 @@
 # Edinburgh bus tracker - ncurses display example
+# (curses is awful though, so the formatting sucks)
 # (c) Mark Pentler 2017
 
 from edinbustrack import *
@@ -6,18 +7,19 @@ from curses import wrapper
 import time
 
 stop_id = "36232626"
+stop_name = get_stop_name(stop_id)
 
 def main(stdscr):
 	services = get_bus_times(stop_id)
 	stdscr.clear()
 
-	stdscr.addstr(0, 0, "Edinburgh bus tracker for busstop " + stop_id + " - CTRL-C to exit")
-	stdscr.addstr(1, 0, "----------------")
-	stdscr.addstr(2, 0, "Service|Mins")
-	stdscr.addstr(3, 0, "----------------")
+	stdscr.addstr(0, 0, "Next departures from " + stop_name + " - CTRL-C to exit")
+	stdscr.addstr(1, 0, "---------------------")
+	stdscr.addstr(2, 0, "| Service |   Mins  |")
+	stdscr.addstr(3, 0, "---------------------")
 
         for id, service, mins in services: # iterate through the list
-		stdscr.addstr(id+4, 0, service + "   " + mins)
+		stdscr.addstr(id+4, 0, "|" + service + "         " + mins)
 
 	stdscr.refresh()
 	time.sleep(30)
